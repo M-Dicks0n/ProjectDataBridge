5affa99929e11edd425d1183dfa2ad283739fe55
Dicks <m.dickson.cs@gmail.com>
2026-04-17 19:05:52 -0400

    Day 2- Data Processing

--- DataBridge.py
+++ DataBridge.py
@@ -1,29 +1,49 @@
 import requests
 import json
 
+
 def fetch_weather_data():
     # Example: Fetching current weather for New York City
-    url = "https://api.open-meteo.com/v1/forecast?latitude=40.7128&longitude=-74.0060&current_weather=true"
+    url = "https://api.open-meteo.com/v1/forecast?latitude=40.7128&longitude=-74.0060&current_weather=true&temperature_unit=fahrenheit&wind_speed_unit=mph"
 
     try:
         response = requests.get(url)
-        # Check if the request was successful
         response.raise_for_status()
-        # Add your visual confirmation here!
+        #  visual confirmation
         print("Status: 200 OK - Connection Successful!\n")
         return response.json()
     except requests.exceptions.RequestException as e:
-        print(f"Error fetching data: {e}")
+        print(f"Error with Connection: {e}")
         return None
 
 
-if __name__ == "__main__":
-    print("Initiating API connection...")
-    weather_payload = fetch_weather_data()
+def transform_data(raw_json):
+    print("Processing Data...")
 
-    if weather_payload:
-        # Format and print the payload if the fetch was successful
-        formatted_json = json.dumps(weather_payload, indent=4)
-        print(formatted_json)
+    current_node = raw_json.get("current_weather", {})
+    clean_temp = float(current_node.get("temperature", 0.0))
+    ''' TODO add real feel
+    #clean_feel = float(current_node.get("apparent_temperature",
+                                        0.0))  
+    '''
+    clean_wind = float(current_node.get("windspeed", 0.0))
+    raw_time = current_node.get("time", "2020-01-01T00:00:00Z")
+    clean_time = raw_time.replace("T", " ")
+
+    transformed_data = {
+        "Time": clean_time,
+        "Temperature": clean_temp,
+        "Wind_speed": clean_wind,
+    }
+    return transformed_data
+
+
+if __name__ == "__main__":
+    print("Initiating Connection...")
+    weather_data = fetch_weather_data()
+
+    if weather_data:
+        clean_data = transform_data(weather_data)
+        print(json.dumps(clean_data, indent=4))
     else:
-        print("Pipeline failed to retrieve data.")
+        print("Error Retrieving Data.")
